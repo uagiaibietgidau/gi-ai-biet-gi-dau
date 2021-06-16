61f4f9e6f3f1138bb56ea8a9003a3daf4c1d2d5a
lovetoyou <85979641+lovetoyou@users.noreply.github.com>
2021-06-16 11:34:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    desc: 'Mày coi t là gì?? ',
+    btnYes: 'Gì ai biết chắc bồ =)))',
+    btnNo: 'Không, pet của t',
+    question:'=)) Ủa rồi chọn cái này thế',
+    btnReply: 'Gì ai biết chắc bồ =)))',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/messages/t/100024334722679'
 }
